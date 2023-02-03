5621d497c5abda5a0929d5069e392bc4ebfd8578
mahdirahmani1376 <rahmanimahdi20@yahoo.com>
2023-02-03 15:13:37 +0330

    1

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,19 +2,27 @@
 
 @section('content')
 
-    <div class="flex items-center mb-3">
-        <h1 class="mr-auto">birdboard</h1>
-        <a href="{{ route('projects.create') }}"> new project</a>
-    </div>
-    <div class="flex">
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-end w-full ">
+
+            <h2 class="text-gray-700 font-normal text-lg rounded py-4 text-white bg-blue-700 px-2">my projects</h2>
+
+            <a class="rounded py-4 text-white bg-blue-700 px-2" href="{{ route('projects.create') }}"> new project</a>
+        </div>
+    </header>
+    <main class="lg:flex lg:flex-wrap -mx-3">
             @forelse($projects as $project)
                 <div class="mx-auto bg-white mr-4 p-5 shadow-2xl rounded">
-                    <h3>{{ $project->title }}</h3>
+                    <h3 class="font-normal text-xl py-4">
+                        <a href="{{ route('projects.show',$project->id) }}">
+                            {{ $project->title }}
+                        </a>
+                    </h3>
 
-                    <div>{{ Str::limit($project->description) }}</div>
+                    <div class="text-gray-600">{{ Str::limit($project->description) }}</div>
                 </div>
             @empty
                 <li>no projects yet</li>
             @endforelse
-    </div>
+    </main>
 @endsection
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{ $project->title }}</h1>
-    <p>{{ $project->description }}</p>
+
+    <h2>tasks</h2>
+    <h2>general notes</h2>
+    <main>
+        <h1>{{ $project->title }}</h1>
+        <p>{{ $project->description }}</p>
+    </main>
 @endsection
